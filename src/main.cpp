3503850c6cb4c79fa7c101f5c5bda2d0f4ba5ca7
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-02-18 16:58:46 +0700

    Program stub

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,30 @@
-int main()
+#include <argparse/argparse.hpp>
+#include <memory>
+
+std::shared_ptr<const argparse::ArgumentParser> parse_arguments(int argc, char* argv[])
 {
+    auto parser = std::make_shared<argparse::ArgumentParser>("rwconvert");
+
+    parser->add_argument("input")
+        .help("RenderWare file to convert");
+
+    try
+    {
+        parser->parse_args(argc, argv);
+    }
+    catch (const std::exception& err)
+    {
+        std::cerr << err.what() << std::endl;
+        std::cerr << *parser << std::endl;
+        exit(1);
+    }
+
+    return parser;
+}
+
+int main(int argc, char* argv[])
+{
+    auto arguments = parse_arguments(argc, argv);
+    
     return 0;
 }
